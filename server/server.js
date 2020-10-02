fb3d9f5f192c77900b9aebc57d5be11aa8a489d3
dogurasu <dugs134@gmail.com>
2020-10-02 16:01:13 -0700

    started to build v1 of API

--- server/server.js
+++ server/server.js
@@ -4,8 +4,43 @@ const express = require("express");
 // intiailize node app
 const app = express();
 
+// middleware
+app.use((req, res, next) => {
+    // res.status(404).json({
+    //     status: 'fail'
+    // });
+});
+
+// routes
+// get all restaurants
+app.get("/api/v1/restaurants", (req, res) => {
+    console.log("route handler ran");
+    res.status(200).json({
+        status: "success",
+        data: {
+            restaurant: ["mcdonalds", "wendys"]
+        }
+    });
+});
+
+// get a single restaurant
+app.get("/api/v1/restaurants/:restaurant_id", (req, res) => {
+    console.log(req.params);
+    // res.status(200).json({
+    //     status: "success",
+    //     data: {
+
+    //     }
+    // })
+});
+
+// create a restaurant
+app.post("/api/v1/restaurants", (req, res) => {
+    console.log(req);
+});
+
 // import port from dotenv
-const port = process.env.PORT;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
-    console.log(`Server is up and listening on port ${port}`)
+    console.log(`Server is up and listening on port ${port}`);
 });
